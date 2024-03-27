bc81f8eb51477aaf4968eb9d07cd1585a4d6c4ec
harrisonfloam <harrisonfloam@gmail.com>
2024-03-27 09:34:43 -0400

    create four peaks generator class

---
+++ mlrose_hiive/generators/four_peaks_generator.py
@@ -0,0 +1,15 @@
+""" Classes for defining optimization problem objects."""
+
+import numpy as np
+
+from mlrose_hiive import DiscreteOpt, FourPeaks
+
+
+class FourPeaksGenerator:
+    """Generator class for Four Peaks."""
+    @staticmethod
+    def generate(seed, size=20, t_pct=0.1):
+        np.random.seed(seed)
+        fitness = FourPeaks(t_pct=t_pct)
+        problem = DiscreteOpt(length=size, fitness_fn=fitness)
+        return problem
