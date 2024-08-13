0a0b28effcf051613aacbc832c5074784bb4399c
Kyle Nakamura <knakamura13dev@gmail.com>
2024-08-13 06:18:41 -0700

    Add six peaks generator

--- mlrose_hiive/generators/__init__.py
+++ mlrose_hiive/generators/__init__.py
@@ -4,13 +4,10 @@
 # License: BSD 3 clause
 
 from .continuous_peaks_generator import ContinuousPeaksGenerator
-
 from .flip_flop_generator import FlipFlopGenerator
-
+from .four_peaks_generator import FourPeaksGenerator
 from .knapsack_generator import KnapsackGenerator
-
 from .max_k_color_generator import MaxKColorGenerator
-
 from .queens_generator import QueensGenerator
-
+from .six_peaks_generator import SixPeaksGenerator
 from .tsp_generator import TSPGenerator
