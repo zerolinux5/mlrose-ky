d7ef58aae24798d6e073fc13a6cfccfa127130b6
gitongah <mbaabugitonga@gmail.com>
2024-08-28 01:27:43 -0700

    Changed the import class names to the correct names

--- src/mlrose_ky/algorithms/decay/__init__.py
+++ src/mlrose_ky/algorithms/decay/__init__.py
@@ -3,7 +3,7 @@
 # Author: Genevieve Hayes
 # License: BSD 3-clause
 
-from .arithmetic_decay import ArithmeticDecay
-from .custom_decay import CustomDecay
-from .exponential_decay import ExponentialDecay
-from .geometric_decay import GeometricDecay, GeomDecay
+from .arithmetic_decay import ArithDecay
+from .custom_decay import CustomSchedule
+from .exponential_decay import ExpDecay
+from .geometric_decay import GeomDecay
