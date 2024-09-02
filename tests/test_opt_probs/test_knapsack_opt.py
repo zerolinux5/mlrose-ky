fade3e03ffbb88c347eeb5cb9a051dc1252b0019
Kyle Nakamura <knakamura13dev@gmail.com>
2024-09-02 00:52:14 -0700

    test: add unit tests for KnapsackOpt class in test_knapsack_opt.py

---
+++ tests/test_opt_probs/test_knapsack_opt.py
@@ -0,0 +1,20 @@
+"""Unit tests for opt_probs/knapsack_opt.py"""
+
+# Author: Genevieve Hayes (modified by Kyle Nakamura)
+# License: BSD 3-clause
+
+try:
+    import mlrose_ky
+except ImportError:
+    import sys
+
+    sys.path.append("../..")
+    import mlrose_ky
+
+from mlrose_ky.opt_probs import KnapsackOpt
+
+
+class TestKnapsackOpt:
+    """Tests for KnapsackOpt class."""
+
+    # TODO: add tests for KnapsackOpt
