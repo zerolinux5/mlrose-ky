108547a7c84a87e64cacf39861e14fd0a6ae3394
Kyle Nakamura <knakamura13dev@gmail.com>
2024-09-02 00:39:31 -0700

    test: add unit tests for FlipFlopOpt class in test_flip_flop_opt.py

---
+++ tests/test_opt_probs/test_flip_flop_opt.py
@@ -0,0 +1,20 @@
+"""Unit tests for opt_probs/test_flip_flop_opt.py"""
+
+# Author: Genevieve Hayes (modified by Kyle Nakamura)
+# License: BSD 3-clause
+
+try:
+    import mlrose_ky
+except ImportError:
+    import sys
+
+    sys.path.append("../..")
+    import mlrose_ky
+
+from mlrose_ky.opt_probs import FlipFlopOpt
+
+
+class TestFlipFlopOpt:
+    """Tests for FlipFlopOpt class."""
+
+    # TODO: add tests for FlipFlopOpt
