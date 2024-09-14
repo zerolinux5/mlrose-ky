c441d7ce6c64527c1f6f54285507aa2a2d747b0e
Kyle Nakamura <knakamura13dev@gmail.com>
2024-09-14 01:52:47 -0700

    Revert method, prop, attr names: .neural (#7)

--- src/mlrose_ky/neural/__init__.py
+++ src/mlrose_ky/neural/__init__.py
@@ -3,10 +3,13 @@
 # Authors: Genevieve Hayes (modified by Andrew Rollings, Kyle Nakamura)
 # License: BSD 3-clause
 
-from .activation import identity, relu, leaky_relu, sigmoid, softmax, tanh
-from .fitness.network_weights import NetworkWeights
 from .linear_regression import LinearRegression
 from .logistic_regression import LogisticRegression
 from .neural_network import NeuralNetwork
 from .nn_classifier import NNClassifier
+
+from .activation import identity, relu, leaky_relu, sigmoid, softmax, tanh
+
+from .fitness.network_weights import NetworkWeights
+
 from .utils import flatten_weights, unflatten_weights
